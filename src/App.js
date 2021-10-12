f010d5c779382b1f796839ce2d85cb34356974c2
MJanowskiDev <mat.krz.janowski@gmail.com>
2021-10-12 19:15:38 +0000

    Changes after code review, reformated code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [keyV4,] = useState(uuidv4())
+  const [keyV4] = useState(uuidv4());
   return (
-    <div className="App">
-            <h1>Generator UUID V4</h1>
-      <div className = 'uuidKey'>
+    <div className='App'>
+      <h1>Generator UUID V4</h1>
+      <div className='uuidKey'>
         <p>Twój kod UUID:</p>
-        <h2 className = 'uuidKey__header'>{keyV4}</h2>
-        <p className = 'uuid__paragraph--footer'>Odśwież stronę aby wygenerować nowy klucz.</p>
+        <h2 className='uuidKey__header'>{keyV4}</h2>
+        <p className='uuid__paragraph--footer'>
+          Odśwież stronę aby wygenerować nowy klucz.
+        </p>
       </div>
     </div>
   );
