62b57aff2da098e7bbc0970ff6eaf3ce4eb98b89
MJanowskiDev <mat.krz.janowski@gmail.com>
2021-10-13 14:33:13 +0000

    Added regenerate key button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const [keyV4] = useState(uuidv4());
+  const [keyV4, setKeyV4] = useState(uuidv4());
+  const refreshKey = () => {
+    setKeyV4(uuidv4());
+  };
   return (
     <div className='App'>
       <h1>Generator UUID V4</h1>
@@ -11,8 +14,12 @@ function App() {
         <p>Twój kod UUID:</p>
         <h2 className='uuidKey__header'>{keyV4}</h2>
         <p className='uuid__paragraph--footer'>
-          Odśwież stronę aby wygenerować nowy klucz.
+          Odśwież stronę aby wygenerować nowy klucz, lub kliknij przycisk
+          poniżej.
         </p>
+        <button className='uuidKey__button' onClick={refreshKey}>
+          Generuj
+        </button>
       </div>
     </div>
   );
